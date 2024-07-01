4328aac8e44b40deeace785a91be186590fa6d42
Moses Enyinnaya <moscolyno98@gmail.com>
2024-07-01 02:47:00 +0200

    added lazy loading image to improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const MOVIES = [
   {
@@ -110,6 +110,21 @@ const MOVIES = [
 const HomePage = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const imgRef = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    const nextIndex = (currentIndex + 1) % MOVIES.length;
+    const src = MOVIES[nextIndex].cover;
+
+    const loadNextImage = () => {
+      if (imgRef.current) {
+        imgRef.current.src = src;
+      }
+    };
+
+    loadNextImage();
+  }, [currentIndex]);
+
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % MOVIES.length);
@@ -119,149 +134,160 @@ const HomePage = () => {
   }, []);
 
   return (
-    <AnimatePresence mode="wait">
-      {MOVIES.map(
-        (movie, i) =>
-          currentIndex === i && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{
-                opacity: 1,
-                transition: {
-                  duration: 1.2
-                }
-              }}
-              exit={{
-                opacity: 0,
-                transition: {
-                  duration: 2,
-                  delay: 1.6
-                }
-              }}
-              className="h-dvh w-dvw overflow-hidden fixed flex items-center justify-center"
-              key={i}
-            >
+    <>
+      <AnimatePresence mode="wait">
+        {MOVIES.map(
+          (movie, i) =>
+            currentIndex === i && (
               <motion.div
-                className="box-content absolute h-dvh w-dvw py-6"
-                style={{
-                  backgroundImage: `url(${movie.cover})`,
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat"
-                }}
-                initial={{
-                  x: 200 * (i % 2 === 0 ? 1 : -1),
-                  paddingLeft: i % 2 === 0 ? 400 : 0,
-                  paddingRight: i % 2 !== 0 ? 400 : 0
-                }}
+                initial={{ opacity: 0 }}
                 animate={{
-                  x: 0
+                  opacity: 1,
+                  transition: {
+                    duration: 1.2
+                  }
                 }}
-                transition={{
-                  duration: 14,
-                  ease: "easeInOut"
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    duration: 2,
+                    delay: 1.6
+                  }
                 }}
-              ></motion.div>
+                className="h-dvh w-dvw overflow-hidden fixed flex items-center justify-center"
+                key={i}
+              >
+                <motion.div
+                  className="box-content absolute h-dvh w-dvw py-6"
+                  style={{
+                    backgroundImage: `url(${movie.cover})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                  }}
+                  initial={{
+                    x: 200 * (i % 2 === 0 ? 1 : -1),
+                    paddingLeft: i % 2 === 0 ? 400 : 0,
+                    paddingRight: i % 2 !== 0 ? 400 : 0
+                  }}
+                  animate={{
+                    x: 0
+                  }}
+                  transition={{
+                    duration: 14,
+                    ease: "easeInOut"
+                  }}
+                ></motion.div>
 
-              <div className="fixed w-full h-full left-0 bg-gradient-to-b from-transparent to-black md:to-black/50 px-3 py-6 md:p-[4rem] flex items-end z-30">
-                <div className="w-full flex flex-col md:flex-row md:items-end justify-between gap-20 md:gap-0">
-                  <div className="flex flex-col gap-6 md:gap-10">
-                    <motion.div
-                      initial={{ opacity: 0 }}
-                      animate={{
-                        opacity: 1,
-                        transition: {
-                          duration: 0.5,
-                          delay: 2
-                        }
-                      }}
-                      exit={{
-                        opacity: 0,
-                        transition: {
-                          duration: 0.3,
-                          delay: 1.5
-                        }
-                      }}
-                      // transition={{
+                <div className="fixed w-full h-full left-0 bg-gradient-to-b from-transparent to-black md:to-black/50 px-3 py-6 md:p-[4rem] flex items-end z-30">
+                  <div className="w-full flex flex-col md:flex-row md:items-end justify-between gap-20 md:gap-0">
+                    <div className="flex flex-col gap-6 md:gap-10">
+                      <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{
+                          opacity: 1,
+                          transition: {
+                            duration: 0.5,
+                            delay: 2
+                          }
+                        }}
+                        exit={{
+                          opacity: 0,
+                          transition: {
+                            duration: 0.3,
+                            delay: 1.5
+                          }
+                        }}
+                        // transition={{
 
-                      //   ease: "easeInOut"
-                      // }}
-                    >
-                      <div className="flex items-center gap-1.5 font-medium">
-                        <Image
-                          src={"/symbol.png"}
-                          width={35}
-                          height={68}
-                          alt=""
-                        />
+                        //   ease: "easeInOut"
+                        // }}
+                      >
+                        <div className="flex items-center gap-1.5 font-medium">
+                          <Image
+                            src={"/symbol.png"}
+                            width={35}
+                            height={68}
+                            alt=""
+                          />
 
-                        <p className="text-lg tracking-[0.3em]">
-                          {movie.isSeries ? "SERIES" : "FILM"}
-                        </p>
-                      </div>
-
-                      <div className="px-3 mt-0">
-                        <Image
-                          src={movie.logo.url}
-                          width={movie.logo.width}
-                          height={movie.logo.height}
-                          alt=""
-                          className="overflow-hidden"
-                          priority
-                        />
-                      </div>
-                    </motion.div>
-
-                    <motion.div
-                      initial={{ opacity: 0 }}
-                      animate={{
-                        opacity: 1,
-                        transition: {
-                          duration: 0.5,
-                          delay: 2.6
-                        }
-                      }}
-                      exit={{
-                        opacity: 0,
-                        transition: {
-                          duration: 0.3,
-                          delay: 1.1
-                        }
-                      }}
-                      className="px-2 text-xl flex items-center gap-4"
-                    >
-                      {movie.tags.map((tag, i) => (
-                        <div className="flex items-center gap-4" key={i}>
-                          <p>{tag}</p>
-                          {i + 1 !== movie.tags.length && (
-                            <span className="bg-gray-400 h-2 w-2 rounded-full"></span>
-                          )}
+                          <p className="text-lg tracking-[0.3em]">
+                            {movie.isSeries ? "SERIES" : "FILM"}
+                          </p>
                         </div>
-                      ))}
-                    </motion.div>
-                  </div>
-                  <div>
-                    <Image
-                      src={"/logo.png"}
-                      className="overflow-hidden"
-                      width={200}
-                      height={200}
-                      alt=""
-                      priority
-                    />
 
-                    <p className="text-sm md:text-center text-gray-300 font-medium">
-                      <span>Recreated by</span>{" "}
-                      <a href="https://x.com/meandchimso" target="_blank">
-                        @meandchimso
-                      </a>
-                    </p>
+                        <div className="px-3 mt-0">
+                          <Image
+                            src={movie.logo.url}
+                            width={movie.logo.width}
+                            height={movie.logo.height}
+                            alt=""
+                            className="overflow-hidden"
+                            priority
+                          />
+                        </div>
+                      </motion.div>
+
+                      <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{
+                          opacity: 1,
+                          transition: {
+                            duration: 0.5,
+                            delay: 2.6
+                          }
+                        }}
+                        exit={{
+                          opacity: 0,
+                          transition: {
+                            duration: 0.3,
+                            delay: 1.1
+                          }
+                        }}
+                        className="px-2 text-xl flex items-center gap-4"
+                      >
+                        {movie.tags.map((tag, i) => (
+                          <div className="flex items-center gap-4" key={i}>
+                            <p>{tag}</p>
+                            {i + 1 !== movie.tags.length && (
+                              <span className="bg-gray-400 h-2 w-2 rounded-full"></span>
+                            )}
+                          </div>
+                        ))}
+                      </motion.div>
+                    </div>
+                    <div>
+                      <Image
+                        src={"/logo.png"}
+                        className="overflow-hidden"
+                        width={200}
+                        height={200}
+                        alt=""
+                        priority
+                      />
+
+                      <p className="text-sm md:text-center text-gray-300 font-medium">
+                        <span>Recreated by</span>{" "}
+                        <a href="https://x.com/meandchimso" target="_blank">
+                          @meandchimso
+                        </a>
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </motion.div>
-          )
-      )}
-    </AnimatePresence>
+              </motion.div>
+            )
+        )}
+      </AnimatePresence>
+
+      <Image
+        src={"/bg.jpg"}
+        hidden
+        ref={imgRef}
+        alt=""
+        sizes="(min-width: 808px) 50vw, 100vw"
+        fill
+      />
+    </>
   );
 };
 
